2be3e1ed94e907978b8b8314bf7fd22576a295d4
yujuck <yujin3278@gmail.com>
2022-03-14 21:49:28 +0900

    컴포넌트

---
+++ src/Fruits.svelte
@@ -0,0 +1,27 @@
+<script>
+    // Props
+    // export는 부모-자식 간에 props를 주고 받는 통로라고 생각하면 덜 헷갈림
+    export let fruits;
+    export let reverse;
+    export let slice;
+
+    let computedFruits = [];
+    let name = '';
+
+    if (reverse) {
+        computedFruits = [...fruits].reverse();
+        name = 'reverse';
+    } else if (slice){
+        computedFruits = fruits.slice(...slice.split(","));
+        name = `slice ${slice}`;
+    } else {
+        computedFruits = fruits;
+    }
+</script>
+
+<h2>Fruits {name}</h2>
+<ul>
+    {#each computedFruits as fruit}
+        <li>{fruit}</li>
+    {/each}
+</ul>
---
+++ src/test5.svelte
@@ -0,0 +1,38 @@
+<script>
+    import Fruits from './Fruits.svelte';
+
+    let fruits = ['Apple', 'Banana', 'Cherry', 'Orange', 'Mango'];
+</script>
+
+<!--props로 fruits 전달-->
+<Fruits fruits={fruits} />
+<!-- <Fruits {fruits} />  속성과 값의 이름이 같으면 생략 가능-->
+<Fruits {fruits} reverse={true} />
+<!-- <Fruits {fruits} reverse /> 속성 이름을 작성하는 것 자체가 속성을 전달하는 것과 같으므로 위의 줄과 같은 코드가 됨-->
+<Fruits {fruits} slice="-2" />
+<Fruits {fruits} slice="0, 3" />
+
+<!--
+<h2>Fruits</h2>
+<ul>
+    {#each fruits as fruit}
+        <li>{fruit}</li>
+    {/each}
+</ul>
+
+<h2>Fruits Reverse</h2>
+<ul>
+    ## reverse는 원본을 바꿔버리기 때문에 전개 연산자로 fruits 풀어서 사용
+    {#each [...fruits].reverse() as fruit}
+        <li>{fruit}</li>
+    {/each}
+</ul>
+
+<h2>Fruits slice -2</h2>
+<ul>
+    {#each fruits.slice(-2) as fruit}
+        <li>{fruit}</li>
+    {/each}
+</ul>
+-->
+<!--fruit를 사용하는 구조가 반복됨 => 컴포넌트화 시킬 수 있음-->
